f45bd1bcdaafcda9e4f87e799f3f242384ca6eff
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2026-01-21 20:55:33 +0530

    fix: theming bug, change brand name to TheDefectiveDetective

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
             }]
         ],
         shikiConfig: {
-            theme: "catppuccin-mocha",
+            theme: "gruvbox-dark-hard",
             wrap: true,
         }
     },
