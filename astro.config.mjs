f066e2a7b4e09405c2b3f05e80f5063bebd9a9f5
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2026-01-30 19:59:54 +0530

    chore: add feynman quote + new talk metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,19 @@ import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx()],
+	integrations: [mdx()], // to support MDX files
+	/**
+	 * Okay so apparently, say I am running in localhost, and I have a path that goes "./something", then depending on how the link is perceived, I'll have different behaviours
+	 * If I am at /talks, and I do href to ./hello, then I'll end up at /hello
+	 * But if I am at /talks/ and I do href to ./hello, then I'll end up at /talks/hello
+	 * This issue won't be for remote because GitHub Pages forces each page to be viewed as a directory for each and every one of 'em (0.0)
+	 * 
+	 * NOTE: Gotta remove relative paths, need root relative paths from now on.
+	 */
+	trailingSlash: "always",
+	build: {
+		format: "preserve",
+	},
 
 	markdown: {
 		remarkPlugins: [remarkMath],
