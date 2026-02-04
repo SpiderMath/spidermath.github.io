5e8ff3c8b8fd558abfb61d7e3efbd28fd11b3841
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2026-02-05 01:19:38 +0530

    feat: add sitemap + update footer github link (to go to repo)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,42 +2,35 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from '@tailwindcss/vite';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx()], // to support MDX files
-	/**
-	 * Okay so apparently, say I am running in localhost, and I have a path that goes "./something", then depending on how the link is perceived, I'll have different behaviours
-	 * If I am at /talks, and I do href to ./hello, then I'll end up at /hello
-	 * But if I am at /talks/ and I do href to ./hello, then I'll end up at /talks/hello
-	 * This issue won't be for remote because GitHub Pages forces each page to be viewed as a directory for each and every one of 'em (0.0)
-	 * 
-	 * NOTE: Gotta remove relative paths, need root relative paths from now on.
-	 */
-	trailingSlash: "always",
-	build: {
-		format: "preserve",
-	},
+    integrations: [
+		sitemap(), // to support MDX files
+		mdx(), // to support MDX files
+	],
 
-	markdown: {
-		remarkPlugins: [remarkMath],
-		rehypePlugins: [
-			[rehypeKatex, {
-				// we can define custom LaTeX macros here
-			}]
-		],
-		shikiConfig: {
-			theme: "gruvbox-dark-hard",
-			wrap: true,
-		}
-	},
+    markdown: {
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [
+            [rehypeKatex, {
+                // we can define custom LaTeX macros here
+            }]
+        ],
+        shikiConfig: {
+            theme: "gruvbox-dark-hard",
+            wrap: true,
+        }
+    },
 
-	site: "https://spidermath.github.io",
+    site: "https://spidermath.github.io",
+	// site:"http://localhost:4321",
 
-	vite: {
-		plugins: [tailwindcss()]
-	}
+    vite: {
+        plugins: [tailwindcss()]
+    }
 });
